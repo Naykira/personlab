20c3f333cc72b91aa23286a62a3185c71a6cfa70
Minsub Sim <minsubsim@gmail.com>
2018-06-23 22:32:43 +0900

    fff

--- config.py
+++ config.py
@@ -3,47 +3,17 @@ import numpy as np
 TAR_H = 400
 TAR_W = 400
 
-NUM_KP = 15
-KP_NAMES = ['nose', 'left_eye', 'right_eye', 'left_ear', 'right_ear',
-    'left_shoulder', 'right_shoulder', 'left_elbow', 'right_elbow', 'left_wrist',
-    'right_wrist', 'left_hip', 'right_hip', 'left_knee', 'right_knee',
-    'left_ankle', 'right_ankle']
-KP_LB = 5
-FACE_KP_LB = 0
-BODY_KP_LB = 0
-AREA_LB = 4000
 
-NUM_EDGE = 19
-EDGES = np.array([
-    (0, 1),
-    (0, 2),
-    (1, 2),
-    (1, 3),
-    (2, 4),
-
-    (3, 5),
-    (4, 6),
-    (5, 7),
-    (6, 8),
-    (7, 9),
-
-    (8, 10),
-    (5, 11),
-    (6, 12),
-    (11, 13),
-    (12, 14),
-
-    (13, 15),
-    (14, 16),
-    (5, 6),
-    (11, 12)
-])
+NUM_KP = 16
+NUM_EDGE = 15
+MAX_FRAME_SIZE = 10
+NUM_RECURRENT = 2
 
 RADIUS = 32
 
-COCO_FILES = []
-COCO_FILES.append(('train2017','annotations/person_keypoints_train2017.json', 'train2017'))
-#COCO_FILES.append(('val2017', '../KerasPersonLab/ANNO_FILE/person_keypoints_val2017.json', '../KerasPersonLab/IMG_DIR/val2017/'))
+FACE_KP_LB = 0
+BODY_KP_LB = 0
+AREA_LB = 4000
 
 NUM_GPUS = 1
 BATCH_SIZE = 1
@@ -59,7 +29,6 @@ SURREAL_W = 320
 TRAIN_DATA_BASE_DIR = '/home/ubuntu/personlab/cmu/train/'
 VAL_DATA_BASE_DIR = '/home/ubuntu/personlab/cmu/val/'
 
-
 SURREAL_KP_MAP = [
     15, #턱 0
     12, #목 1
@@ -70,7 +39,7 @@ SURREAL_KP_MAP = [
     1, 4, 7, #왼쪽 엉덩이, 무릎, 발목 10 11 12
     2, 5, 8, #왼쪽 엉덩이, 무릎, 발목 13 14 15
 ]
-SURREAL_EDGE = np.array([
+EDGES = np.array([
     (0, 1),
     (1, 2),
     (2, 3),
@@ -87,9 +56,3 @@ SURREAL_EDGE = np.array([
     (13, 14),
     (14, 15),
 ])
-EDGES = SURREAL_EDGE
-
-NUM_KP = 16
-NUM_EDGE = 15
-MAX_FRAME_SIZE = 10
-NUM_RECURRENT = 2
---
+++ tensor_info.py
@@ -0,0 +1,58 @@
+import tensorflow as tf
+import config
+
+INPUT_TENSOR_INFO = [
+    {
+        'name': 'image',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, 3),
+        'type': tf.float32,
+    },{
+        'name': 'hm',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.float32,
+    },{
+        'name': 'so_x',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.float32,
+    },{
+        'name': 'so_y',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.float32,
+    },{
+        'name': 'mo_x',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_EDGE),
+        'type': tf.float32,
+    },{
+        'name': 'mo_y',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_EDGE),
+        'type': tf.float32,
+    },{
+        'name': 'seq_len',
+        'shape': (config.BATCH_SIZE,),
+        'type': tf.int32,
+    }
+]
+
+OUTPUT_TENSOR_INFO = [
+    {
+        'name': 'hm',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.int32,
+    },{
+        'name': 'so_x',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.float32,
+    },{
+        'name': 'so_y',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_KP),
+        'type': tf.float32,
+    },{
+        'name': 'mo_x',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_EDGE),
+        'type': tf.float32,
+    },{
+        'name': 'mo_y',
+        'shape': (config.BATCH_SIZE, config.MAX_FRAME_SIZE, config.TAR_H, config.TAR_W, config.NUM_EDGE),
+        'type': tf.float32,
+    }
+]
